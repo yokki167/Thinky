09f4fefc9ffe8b960faa90d1bcdb3260bcddf6d9
SomaSekimoto <0421.soma@gmail.com>
2020-04-21 11:36:37 +0900

    Add: makeStyles

--- thinky_front/src/components/Home.js
+++ thinky_front/src/components/Home.js
@@ -1,58 +1,65 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import home from "../styles/Home.module.css";
+import HomeCss from "../styles/Home.module.scss";
 import { makeStyles } from "@material-ui/core/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000",
-    },
-  },
-});
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       light: "#757ce8",
+//       main: "#3f50b5",
+//       dark: "#002884",
+//       contrastText: "#fff",
+//     },
+//     secondary: {
+//       light: "#ff7961",
+//       main: "#f44336",
+//       dark: "#ba000d",
+//       contrastText: "#000",
+//     },
+//   },
+// });
 
 const useStyles = makeStyles({
+  head: {
+    color: "white",
+    margin: "0 auto",
+  },
+
   home: {
-    backgroundColor: "black",
+    backgroundColor: "gray",
     height: "100vh",
     padding: "100px",
   },
 
   container: {
-    backgroundColor: "blanchedalmond",
     height: "100%",
     alignItems: "center",
     margin: "0 auto",
-    display: "block",
+    display: "flex",
   },
 
   btnWrapper: {
     height: "50%",
+    width: "75%",
     alignItems: "center",
-    backgroundColor: "red",
     margin: "0 auto",
+    display: "block",
   },
 
   btn: {
-    width: "75%",
+    // width: "75%",
     display: "block",
-    margin: {
-      left: "auto",
-      right: "auto",
-      top: "60px",
-    },
+    marginTop: "60px",
+    marginLeft: "auto",
+    marginRight: "auto",
     padding: "20px",
+  },
+
+  link: {
+    textDecoration: "none",
   },
 });
 
@@ -61,17 +68,39 @@ const Home = () => {
   return (
     <Router>
       <div className={home.home}>
+        <h2 className={home.head}>What do you want to think?</h2>
         <div className={home.container}>
           <div className={home.btnWrapper}>
-            <Button variant="contained" color="info" className={home.btn}>
-              "Why"を考える
-            </Button>
-            <Button variant="contained" color="primary" className={home.btn}>
-              みんなの"Y"を考える
-            </Button>
-            <Button variant="contained" color="primary" className={home.btn}>
-              マイページ
-            </Button>
+            <Link to="/mine" className={home.link}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={home.btn}
+              >
+                "Why"を考える
+              </Button>
+            </Link>
+            <Link to="/share" className={home.link}>
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={home.btn}
+              >
+                みんなの"Y"を考える
+              </Button>
+            </Link>
+            <Link to="/mypage" className={home.link}>
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={home.btn}
+              >
+                マイページ
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
