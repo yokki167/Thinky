40eb4d35f654adb59439222540551ac53a76bafa
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-04-22 11:56:56 +0900

    Fix: put in <Header />

--- thinky_front/src/components/Header.js
+++ thinky_front/src/components/Header.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
+export default function Header() {
   const classes = useStyles();
 
   return (
--- thinky_front/src/components/Layout.js
+++ thinky_front/src/components/Layout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Header from "./Header";
 import layoutStyles from "../styles/Layout.module.scss";
 
 export default function Layout(props) {
   return (
     <div className={layoutStyles.container}>
-      <Header />
-      <div className={layoutStyles.content}>{props.children}</div>
-      <Footer />
+      <MuiThemeProvider theme={theme}>
+        <Header />
+        <div className={layoutStyles.content}>{props.children}</div>
+        <Footer />
+      </MuiThemeProvider>
     </div>
   );
 }
