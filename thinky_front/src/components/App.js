bc82095498a6b4933ecd6c8375d505e1417ef867
Terada <terada.syun.kim@gmail.com>
2020-04-22 21:47:12 +0900

    🔰 Initial commit from Terada
    1. semicolon removed
    2. small fix in directory orginization
    3. Add little bit of comments on App.js and index.js
    4. ReadME.md Updated with demo photo

---
+++ thinky_front/src/components/App.js
@@ -0,0 +1,33 @@
+// Import Components
+import Home from "./Home.js"
+import Layout from "./Layout"
+// import SignUp from "./components/SignUp";
+// import SignIn from "./components/SignIn";
+import UserMypage from "./UserMypage"
+import EveryoneWhy from "./EveryoneWhy"
+import ChatPage from "./ChatPage"
+// import UserEdit from "./components/UserEdit";
+
+// Import Styles
+import "../styles/App.css"
+
+// Import Pacakages
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+function App() {
+	return (
+		<Router>
+			<Layout>
+				<Switch>
+					<Route path="/home" component={Home} />
+					<Route path="/why" component={ChatPage} />
+					<Route path="/share" component={EveryoneWhy} />
+					<Route path="/mypage" component={UserMypage} />
+				</Switch>
+			</Layout>
+		</Router>
+	)
+}
+
+export default App
--- thinky_front/src/components/Layout.js
+++ thinky_front/src/components/Layout.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import Footer from "../components/Footer";
-import Header from "./Header";
-import layoutStyles from "../styles/Layout.module.scss";
-import { grey } from "@material-ui/core/colors";
+import React from "react"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import Footer from "../components/Footer"
+import Header from "./Header"
+import layoutStyles from "../styles/Layout.module.scss"
+import { grey } from "@material-ui/core/colors"
 
 const theme = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: { light: grey[300], main: grey[800], dark: grey[700] },
-  },
-});
+	palette: {
+		type: "dark",
+		primary: { light: grey[300], main: grey[800], dark: grey[700] },
+	},
+})
 
 export default function Layout(props) {
-  return (
-    <div className={layoutStyles.container}>
-      <MuiThemeProvider theme={theme}>
-        <Header />
-        <div className={layoutStyles.content}>{props.children}</div>
-        <Footer />
-      </MuiThemeProvider>
-    </div>
-  );
+	return (
+		<div className={layoutStyles.container}>
+			<MuiThemeProvider theme={theme}>
+				<Header />
+				<div className={layoutStyles.content}>{props.children}</div>
+				<Footer />
+			</MuiThemeProvider>
+		</div>
+	)
 }
