028cec541fb423e4a3270479c8bf176eab798bc6
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-04-28 12:34:15 +0900

    Fix: Routers again

--- thinky_front/src/components/Home.js
+++ thinky_front/src/components/Home.js
@@ -1,6 +1,6 @@
 // Import Packages
 import React, { useState } from "react"
-import { BrowserRouter as Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 // Import Styles
 import Button from "@material-ui/core/Button"
