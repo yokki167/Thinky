45e382a16e51f45b6ae8f796efe89a8eafbf6171
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-04-30 22:30:16 +0900

    Add: scrollDown()

--- thinky_front/src/components/Answer.js
+++ thinky_front/src/components/Answer.js
@@ -40,11 +40,28 @@ function PbAnswer(props) {
   )
 }
 
+function scrollDown() {
+  // ブラウザ間の差異をカバーする
+  // ページ全体の高さを取得する
+  const scrollHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  )
+  // 一番下までスクロールした時の数値を取得(window.innerHeight分(画面表示領域分)はスクロールをしないため引く)
+  const pageMostBottom = scrollHeight - window.innerHeight
+  window.scroll(0, pageMostBottom)
+}
+
 export default function Answer(props) {
   const [pv, setPv] = useState(false)
 
   useEffect(() => {
     setPv(props.pv)
+    scrollDown()
   }, [])
 
   return <div>{pv ? <PvAnswer {...props} /> : <PbAnswer {...props} />}</div>
